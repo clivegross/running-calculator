9becd5a4e478739b0df52b04588347dc44f2d0cb
Clive <clive.gross1@gmail.com>
2022-08-18 16:32:18 +1000

    updated training zone table

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # running-calculator
 
-A client-side running calculator that estimates training paces and predicts race times based on a recent race result.
+A simple client-side running calculator that estimates training paces and predicts race times based on a recent race result.
 
 The calculator is written in javascript so no server required.
+
+These calculations provide a good ballpark but do not factor in variables such as training volume, experience, age, `D'` or multiple distance race results so will have limited accuracy, particularly in the longer distances >30km.
 
 Race times are predicted using the formula by [Peter Riegel](https://en.wikipedia.org/wiki/Peter_Riegel):
 
@@ -16,6 +18,8 @@ Where:
 
 Training Paces are estimated using percentages of Critical Speed as determined by [Dr Philip Skiba](https://physfarm.com) in his book, [Scientific Training For Endurance Athletes (2022)](http://physfarm.com/new/?p=1438).
 
-Critical Speed is estimated as a range by predicting 30 to 40 minute race pace by rearranging Peter Riegel's formula:
+Critical Speed (and `D'`) cannot be determined from a single race result and should be calculated mathematically from test data using the model proposed by Skiba in the above literature. This calculator estimates a `CS` range by predicting 30 to 40 minute race pace by rearranging Peter Riegel's formula:
 
 `pace = T2/D2 = T2/(D1 x (T2/T1)^0.943)`
+
+A good technical description of the power-duration asymptote (Critical Power, `CP`), work above `CP` (`W'`), the equivalent speed-duration asymptote (Critical Speed, `CS`) and distance above `CS` (`D'`) and why they are useful can be found in the 2017 paper [Critical Power: An Important Fatigue Threshold in Exercise Physiology](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5070974/) by Prof Andy Jones et al.
--- calculator.js
+++ calculator.js
@@ -8,24 +8,72 @@ function calculate()
       this.paceInSeconds = this.timeInSeconds / this.distance; //(s/km)
       this.pace400InSeconds = this.paceInSeconds*0.4; //(s/400m)
       this.paceMileInSeconds = this.paceInSeconds*1.609; //(s/mile)
+      this.speed = this.distance*1000 / this.timeInSeconds; //(m/s)
       this.time = convertNumToTime(this.timeInSeconds); // 'hh:mm:ss'
       this.pace = convertNumToTime(this.paceInSeconds); // 'mm:ss'/km
       this.pace400 = convertNumToTime(this.pace400InSeconds); // 'mm:ss'/400m
       this.paceMile = convertNumToTime(this.paceMileInSeconds); // 'mm:ss'/mile
     }
   }
+
+  class CriticalSpeed {
+    constructor(raceDistance, raceTime) {
+      this.raceDistance = raceDistance; //km
+      this.raceTime = raceTime; //s
+      // critical speed based on race duration
+      this.csTmin = 30*60; // s
+      this.csTmax = 40*60; // s
+      this.paceMinInSeconds = predictV2Riegel(distance, time, this.csTmin); // s/km
+      this.paceMaxInSeconds = predictV2Riegel(distance, time, this.csTmax); // s/km
+      this.paceMin400InSeconds = this.paceMinInSeconds*0.4; //(s/400m)
+      this.paceMax400InSeconds = this.paceMaxInSeconds*0.4; //(s/400m)
+      this.paceMinMileInSeconds = this.paceMinInSeconds*1.609; //(s/mile)
+      this.paceMaxMileInSeconds = this.paceMaxInSeconds*1.609; //(s/mile)
+      this.speedMin = 1000 / this.paceMinInSeconds; //(m/s)
+      this.speedMax = 1000 / this.paceMaxInSeconds; //(m/s)
+      this.paceMin = convertNumToTime(this.paceMinInSeconds); // 'mm:ss'/km
+      this.paceMax = convertNumToTime(this.paceMaxInSeconds); // 'mm:ss'/km
+      this.pace400Min = convertNumToTime(this.paceMin400InSeconds); // 'mm:ss'/km
+      this.pace400Max = convertNumToTime(this.paceMax400InSeconds); // 'mm:ss'/km
+      this.paceMileMin = convertNumToTime(this.paceMinMileInSeconds); // 'mm:ss'/km
+      this.paceMileMax = convertNumToTime(this.paceMaxMileInSeconds); // 'mm:ss'/km
+    }
+  }
+
+  class TrainingZones {
+    constructor(criticalSpeedMin, criticalSpeedMax) {
+      this.criticalSpeedMin = criticalSpeedMin; // s/km
+      this.criticalSpeedMax = criticalSpeedMax; // s/km
+      this.percentCS = {};
+      this.percentCS['E'] = {'min':115, 'max':124};
+      this.percentCS['R'] = {'min':124};
+      this.percentCS['Tempo'] = {'min':105, 'max':115};
+      this.percentCS['T'] = {'min':95, 'max':105};
+      this.percentCS['VO2Max'] = {'min':84, 'max':95};
+      this.percentCS['AnC'] = {'max':84};
+      // this.paceInSeconds = this.timeInSeconds / this.distance; //(s/km)
+      // this.pace400InSeconds = this.paceInSeconds*0.4; //(s/400m)
+      // this.paceMileInSeconds = this.paceInSeconds*1.609; //(s/mile)
+      // this.speed = this.distance*1000 / this.timeInSeconds; //(m/s)
+      // this.time = convertNumToTime(this.timeInSeconds); // 'hh:mm:ss'
+      // this.pace = convertNumToTime(this.paceInSeconds); // 'mm:ss'/km
+      // this.pace400 = convertNumToTime(this.pace400InSeconds); // 'mm:ss'/400m
+      // this.paceMile = convertNumToTime(this.paceMileInSeconds); // 'mm:ss'/mile
+    }
+  }  
  
   // set distance and time from form input
   var distance = document.getElementById("distance").value;
-  var time = document.getElementById("timeHH").value*60*60 + document.getElementById("timeMM").value*60 + document.getElementById("timeSS").value;
+  var time = document.getElementById("timeHH").value*60*60 + document.getElementById("timeMM").value*60 + document.getElementById("timeSS").value*1;
+  console.log(document.getElementById("timeHH").value*60*60);
+  
+  console.log(document.getElementById("timeSS").value);
+  console.log(time);
+  var customRaceDistance = document.getElementById("customDistance").value;
   
   
   // critical speed based on race duration
-  var csTmin = 30*60;
-  var csTmax = 40*60;
-  // csMin = csTmin / (distance*(csTmin/time)**0.943)/60; // (min/km)
-  var csMin = predictV2Riegel(distance, time, csTmin); // s/km
-  var csMax = predictV2Riegel(distance, time, csTmax); // s/km
+  var criticalSpeed = new CriticalSpeed(distance, time);
   
   // race predictions
   let racePredictions = {};
@@ -39,6 +87,7 @@ function calculate()
   racePredictions['1500'] = new RacePrediction(1.5);
   racePredictions['1000'] = new RacePrediction(1);
   racePredictions['800'] = new RacePrediction(0.8);
+  racePredictions['Custom'] = new RacePrediction(customRaceDistance);
  
   // output
   // update predicted time element for each race distance
@@ -47,7 +96,7 @@ function calculate()
     raceDistance = item.id.substring(item.id.search("_")+1)
     try {
       document.getElementById(item.id).innerHTML = racePredictions[raceDistance].time;
-    } catch {}    
+    } catch {}
   });
   // update predicted pace element for each race distance
   const racePaceElements = document.querySelectorAll(`[id^="racePace_"]`);
@@ -73,21 +122,29 @@ function calculate()
       document.getElementById(item.id).innerHTML = racePredictions[raceDistance].paceMile;
     } catch {}    
   });
-  document.getElementById("CSmin").innerHTML = convertNumToTime(csMin);
-  document.getElementById("CSmax").innerHTML = convertNumToTime(csMax);
+  document.getElementById("CSPaceMin").innerHTML = criticalSpeed.paceMin;
+  document.getElementById("CSPaceMax").innerHTML = criticalSpeed.paceMax;
+  document.getElementById("CS400Min").innerHTML = criticalSpeed.pace400Min;
+  document.getElementById("CS400Max").innerHTML = criticalSpeed.pace400Max;
+  document.getElementById("CSMileMin").innerHTML = criticalSpeed.paceMileMin;
+  document.getElementById("CSMileMax").innerHTML = criticalSpeed.paceMileMax;
+  document.getElementById("CSMin").innerHTML = criticalSpeed.speedMin.toFixed(1);
+  document.getElementById("CSMax").innerHTML = criticalSpeed.speedMax.toFixed(1);
 }
 
-
-
-
-function predictD2Riegel(D1, T1, T2) {
-  // Riegel formula for race time prediction:
-  // T2 = T1 x (D2/D1) ^1.06
-  // Rearranging the Rigel formula above for distance
-  // D2 = D1 x (T2/T1)^0.943
-  // https://en.wikipedia.org/wiki/Peter_Riegel
-  D2 = D1 * (T2/T1)**0.943;
-  return D2
+function calcTrainingPaceMetrics(obj, paceMinInSeconds, paceMaxInSeconds) {
+  obj.paceMin400InSeconds = paceMinInSeconds*0.4; //(s/400m)
+  obj.paceMax400InSeconds = paceMaxInSeconds*0.4; //(s/400m)
+  obj.paceMinMileInSeconds = paceMinInSeconds*1.609; //(s/mile)
+  obj.paceMaxMileInSeconds = paceMaxInSeconds*1.609; //(s/mile)
+  obj.speedMin = 1000 / paceMinInSeconds; //(m/s)
+  obj.speedMax = 1000 / paceMaxInSeconds; //(m/s)
+  obj.paceMin = convertNumToTime(paceMinInSeconds); // 'mm:ss'/km
+  obj.paceMax = convertNumToTime(paceMaxInSeconds); // 'mm:ss'/km
+  obj.pace400Min = convertNumToTime(this.paceMin400InSeconds); // 'mm:ss'/km
+  obj.pace400Max = convertNumToTime(this.paceMax400InSeconds); // 'mm:ss'/km
+  obj.paceMileMin = convertNumToTime(this.paceMinMileInSeconds); // 'mm:ss'/km
+  obj.paceMileMax = convertNumToTime(this.paceMaxMileInSeconds); // 'mm:ss'/km
 }
 
 function predictT2Riegel(D1, D2, T1) {
@@ -98,39 +155,42 @@ function predictT2Riegel(D1, D2, T1) {
   return T2
 }
 
+function predictD2Riegel(D1, T1, T2) {
+  // Riegel formula for race time prediction:
+  // T2 = T1 x (D2/D1) ^1.06
+  // https://en.wikipedia.org/wiki/Peter_Riegel
+  // Rearranging the Rigel formula above for distance
+  // D2 = D1 x (T2/T1)^0.943
+  D2 = D1 * (T2/T1)**0.943;
+  return D2
+}
+
 function predictV2Riegel(D1, T1, T2) {
   // Riegel formula for race time prediction:
   // T2 = T1 x (D2/D1)^1.06
-  // Rearranging the Rigel formula above for distance
-  // D2 = D1 x (T2/T1)^0.943
   // https://en.wikipedia.org/wiki/Peter_Riegel
-  // pace [time/distance]
+  // Rearranging the Rigel formula above for pace [time/distance]
   // V2 = (T2)/(D1*(T2/T1)^0.943)
   V2 = T2 / predictD2Riegel(D1, T1, T2);
   return V2
 }
 
 function convertNumToTime(number) {
-  // 'number' represents time/duration in seconds
+  // 'number' represents duration in seconds
+  // returns string "hh:mm:ss" if number >= 1 hour or "mm:ss" if < 1 hour
+
   // Check sign of given number
   var sign = (number >= 0) ? 1 : -1;
 
   // Set positive value of number of sign negative
   number = number * sign;
 
-  // Separate the int from the decimal part
+  // Separate the hours, minutes, seconds
   var hour = Math.floor(number/3600);
   var minute = Math.floor(number/60) - hour*60;
   var second = Math.floor(number) - hour*3600 - minute*60;
-  var decpart = number - hour;
 
-  // var min = 1 / 60;
-  // // Round to nearest minute
-  // decpart = min * Math.round(decpart / min);
-
-  // var minute = Math.floor(decpart * 60) + '';
-
-  // Add padding if need
+  // Add padding if needed
   if (minute.toString().length < 2) {
   minute = '0' + minute; 
   }
@@ -143,13 +203,12 @@ function convertNumToTime(number) {
   sign = sign == 1 ? '' : '-';
 
   if (hour == 0) {
-    // Concate minutes and seconds 'mm:ss'
+    // Concatenate minutes and seconds 'mm:ss'
     time = sign + minute + ':' + second;
   } else {
-    // Concate hours, minutes and seconds 'hh:mm:ss'
+    // Concatenate hours, minutes and seconds 'hh:mm:ss'
     time = sign + hour + ':' + minute + ':' + second;
   }
-
 
   return time;
 }
